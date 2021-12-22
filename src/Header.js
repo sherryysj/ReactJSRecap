dc1db6cdbd3cf5753875693d0aadee2ee62a35aa
sherryysj <45263452+sherryysj@users.noreply.github.com>
2021-12-22 20:37:27 +1100

    learning note: use style directly

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,13 @@
 /*  and use _rafce command to create a generic react arrow function component */
 
 const Header = () => {
+
     return (
-        <header>
+        // use style directly, double {} needed, the inside one refers to the style
+        <header style={{
+            backgroundColor: 'mediumblue',
+            color: '#fff'
+        }}>
             <h1>Welcome</h1>
         </header>
     )
