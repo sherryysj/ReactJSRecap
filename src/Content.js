0861de6c7a7d5eca79a91cdc9736a6e8e6db4a01
sherryysj <45263452+sherryysj@users.noreply.github.com>
2021-12-22 21:14:02 +1100

    learning note: handle click events

--- src/Content.js
+++ src/Content.js
@@ -8,11 +8,32 @@ const Content = () => {
    
     }
 
+    // normal click
+    const handleClick = () => {
+        console.log('You clicked it')
+    }
+
+    // with parameter
+    const handleClick2 = (name) => {
+        // use ` when calling parameters
+        console.log(`${name} was clicked`)
+    }
+
+    // calling event
+    const handleClick3 = (e) => {
+        console.log(e);
+        console.log(e.target); // ';' can be omitted
+        console.log(e.target.innerText)
+    }
+
     return (
         <main>
-            <p>
+            <p onDoubleClick={handleClick}> {/* double click not work in pad device*/}
                 Hello, {handleNameChange()}!
             </p>
+            <button onClick={handleClick}>Click it</button> {/* do not include () after handleClick */}
+            <button onClick={() => handleClick2('Dave')}>Click handle with parameter</button>
+            <button onClick={(e) => handleClick3(e)}>Click handle event</button>
         </main>
     )
 }
