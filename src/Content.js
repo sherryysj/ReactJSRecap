948074afb3272fcc20955584f251fd62cae62738
sherryysj <45263452+sherryysj@users.noreply.github.com>
2021-12-24 16:52:37 +1100

    learning note: if-else handling, call localStorage to save JSON, set handle check and delete function, use item filter

--- src/Content.js
+++ src/Content.js
@@ -32,24 +32,49 @@ const Content = () => {
         }
     ]);
 
+    const handleCheck = (id) => {
+        // change item using map
+        const listItems = items.map((item) => item.id === id ? 
+        {...item, checked: !item.checked} : item);
+        setItems(listItems);
+
+        // save the changed listItems in JSON file
+        localStorage.setItem('gamelist', JSON.stringify(listItems));
+    }
+
+    const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id);
+        setItems(listItems);
+        localStorage.setItem('gamelist', JSON.stringify(listItems));
+    }
+
     // use Tab to auto indentation
     return (
         <main>
+            {items.length ? (
             <ul>
                 {items.map((item) => (
                     <li className="item" key={item.id}>
                         <input
                             type="checkbox"
+                            onChange={() => handleCheck(item.id)}
                             checked={item.checked}
                         />
-                        <label>{item.item}</label>
+                        <label
+                            style={(item.checked) ? {textDecoration:'line-through'} : null}
+                            onDoubleClick={() => handleCheck(item.id)}
+                        >{item.item}</label>
                         <FaTrashAlt 
+                            onClick={() => handleDelete(item.id)}
                             role="button" 
                             tabIndex="0" 
                         />
                     </li>
                 ))}
             </ul>
+            ) : (
+                <p style={{marginTop: '2rem'}}>Your list is empty.</p>
+            )}
         </main>
     )
 }
