1885390768608961f8d0619f54361d26aa7028ae
sherryysj <45263452+sherryysj@users.noreply.github.com>
2021-12-24 16:07:23 +1100

    learning note: use of react icon and update the list style

--- src/Content.js
+++ src/Content.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaTrashAlt } from 'react-icons/fa'
 
 const Content = () => {
 
@@ -42,7 +43,10 @@ const Content = () => {
                             checked={item.checked}
                         />
                         <label>{item.item}</label>
-                        <button>Delete</button>
+                        <FaTrashAlt 
+                            role="button" 
+                            tabIndex="0" 
+                        />
                     </li>
                 ))}
             </ul>
