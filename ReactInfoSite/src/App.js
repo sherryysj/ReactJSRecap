c38401a29e983889da598437de27d28519942860
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-02-11 19:34:35 +1100

    add content component

--- ReactInfoSite/src/App.js
+++ ReactInfoSite/src/App.js
@@ -1,9 +1,11 @@
+import Content from "./Components/Content";
 import Header from "./Components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Content />
     </>
   );
 }
