fbe2d5bce39083d282bcafcec7291bf6a9e39a17
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-02-12 13:17:12 +1100

    add nav bar items

--- ReactInfoSite/src/Components/Header.js
+++ ReactInfoSite/src/Components/Header.js
@@ -4,6 +4,10 @@ function Header() {
         <header>
             <nav>
                 <img src="assets/images/react-logo.png" style={{width:"50px",height:"50px"}} alt="react logo" />
+                <ul>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
             </nav>
         </header>    
     )
