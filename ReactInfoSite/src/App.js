87ffc8af69502172b33cfe6cb35c41892317d609
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-02-12 13:20:44 +1100

    update style of nav bar

--- ReactInfoSite/src/App.js
+++ ReactInfoSite/src/App.js
@@ -1,6 +1,7 @@
 import Content from "./Components/Content";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import "./Style/style.css";
 
 function App() {
   return (
--- ReactInfoSite/src/Components/Header.js
+++ ReactInfoSite/src/Components/Header.js
@@ -2,9 +2,9 @@
 function Header() {
     return(
         <header>
-            <nav>
+            <nav className="nav">
                 <img src="assets/images/react-logo.png" style={{width:"50px",height:"50px"}} alt="react logo" />
-                <ul>
+                <ul className="nav-items">
                     <li>About</li>
                     <li>Contact</li>
                 </ul>
