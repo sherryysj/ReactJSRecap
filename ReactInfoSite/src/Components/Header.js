5104a0561d1f30200551523acc846c470c0a726c
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-02-12 13:26:33 +1100

    move logo style into css file

--- ReactInfoSite/src/Components/Header.js
+++ ReactInfoSite/src/Components/Header.js
@@ -3,7 +3,7 @@ function Header() {
     return(
         <header>
             <nav className="nav">
-                <img src="assets/images/react-logo.png" style={{width:"50px",height:"50px"}} alt="react logo" />
+                <img src="assets/images/react-logo.png" className="nav-logo" alt="react logo" />
                 <ul className="nav-items">
                     <li>About</li>
                     <li>Contact</li>
