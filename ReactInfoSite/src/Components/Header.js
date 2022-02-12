4c4548cb920ac34317282a4c3d800bb97f185646
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-02-12 18:46:15 +1100

    add tutorial link to nav bar

--- ReactInfoSite/src/Components/Header.js
+++ ReactInfoSite/src/Components/Header.js
@@ -4,9 +4,9 @@ function Header() {
         <header>
             <nav className="nav">
                 <img src="assets/images/react-logo.png" className="nav-logo" alt="react logo" />
+                <h3 className="nav-title">ReactFacts</h3>
                 <ul className="nav-items">
-                    <li>About</li>
-                    <li>Contact</li>
+                    <li><a href="https://scrimba.com/learn/learnreact/project-setup-co2104c96a93e1b2ab03254ba">Tutorial</a></li>
                 </ul>
             </nav>
         </header>    
