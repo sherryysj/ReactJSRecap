86a3468fab03e2753de3eefa6b91b3fbae312fa3
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-02-13 15:26:29 +1100

    add contact component and set style initially

--- BusinessCard/src/Components/Card.js
+++ BusinessCard/src/Components/Card.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Contact from './Contact';
 import Header from './Header';
 
 function Card() {
   return (
     <div className='card'>
         <Header />
+        <Contact />
 
     </div>
   )
---
+++ BusinessCard/src/Components/Contact.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className="contact">
+      <div>
+      <a className="linkedin" type="button" href="https://www.linkedin.com/in/shujie-yang-74084a17a/">
+          <i class="fa fa-linkedin-square"></i>
+          Linkedin
+        </a>
+      </div>
+        <div>
+          <a className="github" type="button" href="https://github.com/sherryysj">
+          <i class="fa fa-github"></i>
+          GitHub
+          </a>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Contact
