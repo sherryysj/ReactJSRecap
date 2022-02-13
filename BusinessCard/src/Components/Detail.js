ad6de8a5b20b8b8bfe4e8801318ce4e84adb3e0d
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-02-13 16:30:35 +1100

    update detail styles and contents

--- BusinessCard/src/Components/Detail.js
+++ BusinessCard/src/Components/Detail.js
@@ -1,14 +1,15 @@
 
 function Detail() {
   return (
-    <div>
+    <div className="detail">
         <div>
             <h4>About</h4>
-            <p>I am a full stack software engineer, who is specialised in web developing and game designing as well as implementing.</p>
+            <p>I am a full stack software engineer, who is specialised in web developing with ReactJS and game developing with Unity.</p>
         </div>
         <div>
             <h4>Interests</h4>
-            <p>I am interested in playing strategic and simulated computer games.</p>
+            <p>I am interested in playing strategic and simulated computer games, such as Crusader Kings 3 and Stellaris, 
+                and role-playing games, such as Red Dead Redemption 2 and Assassin's Creed Oringins</p>
         </div>
     </div>
   )
