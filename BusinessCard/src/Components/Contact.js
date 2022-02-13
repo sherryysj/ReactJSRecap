d87e39e85809cc224cf45c0d40109e5821596a9b
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-02-13 15:38:33 +1100

    update contact styles

--- BusinessCard/src/Components/Contact.js
+++ BusinessCard/src/Components/Contact.js
@@ -4,15 +4,15 @@ function Contact() {
   return (
     <div className="contact">
       <div>
-      <a className="linkedin" type="button" href="https://www.linkedin.com/in/shujie-yang-74084a17a/">
+        <a className="linkedin" type="button" href="https://www.linkedin.com/in/shujie-yang-74084a17a/">
           <i class="fa fa-linkedin-square"></i>
-          Linkedin
+          <p>Linkedin</p>
         </a>
       </div>
         <div>
           <a className="github" type="button" href="https://github.com/sherryysj">
-          <i class="fa fa-github"></i>
-          GitHub
+            <i class="fa fa-github"></i>
+            <p>GitHub</p>
           </a>
         </div>
         
