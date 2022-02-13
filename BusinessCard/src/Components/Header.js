b3619a3d5646bde9eed7cc7af0cce5790674591b
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-02-13 16:34:25 +1100

    update header content

--- BusinessCard/src/Components/Header.js
+++ BusinessCard/src/Components/Header.js
@@ -6,7 +6,7 @@ function Header() {
         <img src={Photo} alt="profile" className='photo'/>
         <h2 className='card-name'>Sherry Yang</h2>
         <p className='card-title'>Software Engineer</p>
-        <p className='card-work'>Web Developing</p>
+        <p className='card-work'>Web and Game Developing</p>
     </div>
   )
 }
