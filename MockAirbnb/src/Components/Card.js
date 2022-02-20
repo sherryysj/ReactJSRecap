d068daadeb0fa9be6d1c48ce349dcf0813705196
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-02-20 22:23:57 +1100

    update the way pass props to card

--- MockAirbnb/src/Components/Card.js
+++ MockAirbnb/src/Components/Card.js
@@ -3,12 +3,12 @@ function Card(props) {
     <div className='card'>
         {props.openSpots===0 && <div className="card-spots">SOLD OUT</div>}
         {props.location==="Online" && <div className="card-online">ONLINE</div>}
-        <img src={props.imgLink} alt="profile"/>
+        <img src={`/assets/images/${props.coverImg}`} alt="profile"/>
         <h4>{props.title}</h4>
         <div className="card-detail">
           <div className='card-review'>
               <i class='fa fa-star'></i>
-              <p>{props.rating}<span>({props.reviewCount}) • {props.location}</span></p>
+              <p>{props.stats.rating}<span>({props.stats.reviewCount}) • {props.location}</span></p>
           </div>
           <p className='card-description'>{props.description}</p>
           <div className='card-price'>
--- MockAirbnb/src/Components/CardContainer.js
+++ MockAirbnb/src/Components/CardContainer.js
@@ -7,15 +7,23 @@ function CardContainer() {
     return (
       <Card 
         key={card.id}
-        title={card.title}
-        imgLink={`/assets/images/${card.coverImg}`}
-        openSpots={card.openSpots}
-        rating={card.stats.rating}
-        reviewCount={card.stats.reviewCount}
-        location={card.location}
-        description={card.description}
-        price={card.price}
-        priceType={card.priceType}
+
+        /* pass each element separately - simple way*/
+        {...card}
+
+        /* pass the whole object */
+        //{card}
+
+        /* pass each element separately - more readable way */
+        //title={card.title}
+        //imgLink={`/assets/images/${card.coverImg}`}
+        //openSpots={card.openSpots}
+        //rating={card.stats.rating}
+        //reviewCount={card.stats.reviewCount}
+        //location={card.location}
+        //description={card.description}
+        //price={card.price}
+        //priceType={card.priceType}
       />
     )
   })
